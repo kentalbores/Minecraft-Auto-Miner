5b86039ec9c1d6b2bc5d1bceef609f77addb5264
Sticktama <tgm.mendez@gmail.com>
2024-04-09 19:21:29 +0800

    i add stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+
 public class Main{
     public static void main(String[] args){
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
         new Frame();
     }
 }
